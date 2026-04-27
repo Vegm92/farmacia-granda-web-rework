97d9846535a58be9d969b32e035c44107506d0e6
Vegm92 <victorgranda1992@gmail.com>
2026-04-27 15:14:12 +0200

    config: update next.config with basePath, trailingSlash, and output export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/farmacia-granda-web-rework',
+  images: { unoptimized: true },
+  allowedDevOrigins: ['192.168.1.128'],
 };
 
 export default nextConfig;
